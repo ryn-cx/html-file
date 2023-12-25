bef3e32c1655e23b088a861b5b03c0cb7b3fda97
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-24 23:36:18 -0800

    Updated docstrings

--- html_file/__init__.py
+++ html_file/__init__.py
@@ -14,16 +14,35 @@ class HTMLFile(PavedPath):
     """Library for working with HTML files."""
 
     def __init__(self, *_args: PathableType) -> None:
-        """Initialize the HTMLFile class."""
+        """Initialize the HTMLFile class.
+
+        Args:
+        ----
+            _args: Arguments to pass to the parent class's __init__ method.
+        """
         self.parsed_cached_value = None
         super().__init__()
 
     def parsed(self) -> StrictSoup:
-        """Parse the file bytes using BeautifulSoup."""
+        """Parse the file bytes using BeautifulSoup.
+
+        Returns
+        -------
+            An uncached StrictSoup object.
+        """
         return StrictSoup(self.read_bytes(), "lxml")
 
     def parsed_cached(self, *, reload: bool = False) -> StrictSoup:
-        """Parse the file bytes using BeautifulSoup and cache the result."""
+        """Parse the file bytes using BeautifulSoup and cache the result.
+
+        Args:
+        ----
+            reload: Whether to reload the file bytes and reparse the file.
+
+        Returns:
+        -------
+            A cached StrictSoup object.
+        """
         # I don't know of any reason why you would want to use read_text instead of read_bytes for BeautifulSoup. Unless
         # a specific need arises this function will always just use read_bytes
         if not self.parsed_cached_value or reload:
