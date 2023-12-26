dfb61a04ee82fe5dd4519a8bc07f914a7c6db80e
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2023-12-26 04:16:59 -0800

    Updated for new PavedPath

--- html_file/__init__.py
+++ html_file/__init__.py
@@ -3,11 +3,23 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from paved_path import PavedPath
+from paved_path import CobblestoneCache, PavedPath
 from strict_soup import StrictSoup
 
 if TYPE_CHECKING:
     from paved_path import PathableType
+
+
+class HTMLCache(CobblestoneCache):
+    """Cache for JSONFile.
+
+    This is set up as a seperate class to make it easier to clear the cache.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the cache with None values."""
+        self.parsed: StrictSoup | None = None
+        super().__init__()
 
 
 class HTMLFile(PavedPath):
@@ -20,8 +32,26 @@ class HTMLFile(PavedPath):
         ----
             _args: The path fragments to join together.
         """
-        self.parsed_cached_value = None
-        super().__init__()
+        self.cache = HTMLCache()
+
+    def write(self, content: StrictSoup | str | bytes, *, write_through: bool = True) -> None:
+        """Write the data to the file.
+
+        Args:
+        ----
+            content: The object to be written to the file.
+            write_through: Whether to write through the cache.
+        """
+        # Strings and bytes are not serialized because no changescan be made on them.
+        if isinstance(content, (str, bytes)):
+            super().write(content, write_through=write_through)
+            if write_through:
+                self.cache.parsed = None
+        # All other objects are serialized using json
+        else:
+            super().write(str(content), write_through=write_through)
+            if write_through:
+                self.cache.parsed = content
 
     def parsed(self) -> StrictSoup:
         """Parse the file bytes using BeautifulSoup.
@@ -33,7 +63,7 @@ class HTMLFile(PavedPath):
         return StrictSoup(self.read_bytes(), "lxml")
 
     def parsed_cached(self, *, reload: bool = False) -> StrictSoup:
-        """Parse the file bytes using BeautifulSoup and cache the result.
+        """Parse the file bytes using StrictSoup and cache the result.
 
         Args:
         ----
@@ -41,11 +71,11 @@ class HTMLFile(PavedPath):
 
         Returns:
         -------
-            A cached StrictSoup object.
+            A cached parsed StrictSoup object.
         """
         # I don't know of any reason why you would want to use read_text instead of read_bytes for BeautifulSoup. Unless
         # a specific need arises this function will always just use read_bytes
-        if not self.parsed_cached_value or reload:
-            self.parsed_cached_value = StrictSoup(self.read_bytes_cached(reload=reload), "lxml")
+        if not self.cache.parsed or reload:
+            self.cache.parsed = StrictSoup(self.read_bytes_cached(reload=reload), "lxml")
 
-        return self.parsed_cached_value
+        return self.cache.parsed
--- tests/test_html_file.py
+++ tests/test_html_file.py
@@ -1,31 +1,46 @@
 """Test the HTMLFile class."""
+
+from typing import Generator
+
+import pytest
+from strict_soup import StrictSoup
+
 from html_file import HTMLFile
 
 
 class TestHTMLFile:
     """Test the HTMLFile class."""
 
-    def test_parsed(self) -> None:
-        """Test the parsed function."""
-        file = HTMLFile("tests/test_parsed.html")
-        file.write("<title>Example</title>")
-        assert file.parsed().strict_select_one("title").text == "Example"
-        file.delete()
+    @pytest.fixture()
+    def temporary_file(self) -> Generator[HTMLFile, None, None]:
+        """Get a file path for testing and delete the test_data folder if it exists after the test."""
+        temporary_file = HTMLFile("test_data/file.html")
+        yield temporary_file
+        if temporary_file.parent == HTMLFile("test_data"):
+            temporary_file.parent.delete()
 
-    def test_parsed_cached_changed_file(self) -> None:
-        """Test the parsed_cached function."""
-        file = HTMLFile("tests/test_parsed_cached_changed_file.html")
-        file.write("<title>Example</title>")
-        file.parsed_cached()
-        file.write("")
-        assert file.parsed_cached().strict_select_one("title").text == "Example"
-        file.delete()
+    def test_write(self, temporary_file: HTMLFile) -> None:
+        """Test the write method."""
+        soup = StrictSoup("<title>Example</title>")
+        # Test empty cache without write_through
+        temporary_file.write(soup, write_through=False)
+        assert temporary_file.cache.parsed is None
 
-    def test_parsed_cached_updating_cache(self) -> None:
-        """Test the parsed_cached function."""
-        file = HTMLFile("tests/test_parsed_cached_updating_cache.html")
-        file.write("<title>Example</title>")
-        file.parsed_cached()
-        file.write("<title>Title</title>")
-        assert file.parsed_cached(reload=True).strict_select_one("title").text == "Title"
-        file.delete()
+        # Test empty cache with write_through
+        temporary_file.write(soup)
+        assert temporary_file.cache.parsed == soup
+
+        # Test non-empty cache without write_through
+        temporary_file.write(soup, write_through=False)
+        assert temporary_file.cache.parsed is None
+
+        # Test non-empty cache with write_through
+        temporary_file.write(soup)
+        assert temporary_file.cache.parsed == soup
+
+    def test_parse(self, temporary_file: HTMLFile) -> None:
+        """Test the parse method."""
+        soup = StrictSoup("<title>Example</title>")
+        temporary_file.write(soup)
+        assert temporary_file.parsed() == soup
+        assert temporary_file.cache.parsed == soup
